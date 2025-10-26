f214309e0423545a43a66d10abf815a850acf827
Kaylee Fedorick <kaylee.fedorick@gmail.com>
2025-10-26 10:42:32 -0600

    Use kitty as terminal

--- modules/home-manager/hyprland/configuration.nix
+++ modules/home-manager/hyprland/configuration.nix
@@ -18,12 +18,11 @@ in
   ];
   wayland.windowManager.hyprland.settings = {
     # Default applications
-    "$terminal" = lib.mkDefault "ghostty";
+    "$terminal" = lib.mkDefault "kitty";
     "$fileManager" = lib.mkDefault "nautilus --new-window";
     "$browser" = lib.mkDefault "chromium --new-window --ozone-platform=wayland";
     "$music" = lib.mkDefault "spotify";
-    "$passwordManager" = lib.mkDefault "1password";
-    "$messenger" = lib.mkDefault "signal-desktop";
+    "$passwordManager" = lib.mkDefault "bitwarden";
     "$webapp" = lib.mkDefault "$browser --app";
 
     monitor = cfg.monitors;
---
+++ modules/home-manager/kitty.nix
@@ -0,0 +1,27 @@
+{
+  config,
+  pkgs,
+  ...
+}:
+let 
+  cfg = config.omarchy;
+  palette = config.colorScheme.palette;
+in
+{
+  programs.kitty = {
+    enable = true;
+    themeFile = "Dracula";
+    font.name = cfg.primary_font;
+    font.size = 11;
+    enableGitIntegration = true;
+    settings = {
+      confirm_os_window_close = 0;
+      dynamic_background_opacity = true;
+      enable_audio_bell = false;
+      mouse_hide_wait = "-1.0";
+      window_padding_width = 10;
+      background_opacity = "0.95";
+      background_blur = 5;
+    };
+  };
+}
