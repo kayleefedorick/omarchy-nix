2f8b5eac1ceeab8e1cd4444217abbd5e5738980d
Kaylee Fedorick <kaylee.fedorick@gmail.com>
2025-10-26 20:03:43 -0600

    Set nemo defaults

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -126,5 +126,26 @@ in
   	};
   };
 
+  xdg.desktopEntries.nemo = {
+      name = "Nemo";
+      exec = "${pkgs.nemo-with-extensions}/bin/nemo";
+  };
+  
+  xdg.mimeApps = {
+      enable = true;
+      defaultApplications = {
+          "inode/directory" = [ "nemo.desktop" ];
+          "application/x-gnome-saved-search" = [ "nemo.desktop" ];
+      };
+  };
+
+  dconf = {
+    settings = {
+      "org/cinnamon/desktop/applications/terminal" = {
+        exec = "kitty -e";
+      };
+    };
+  };
+
   programs.neovim.enable = false;
 }
