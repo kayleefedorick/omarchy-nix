ce51096e457b465f23025ede00ac572c15d10a5f
Kaylee Fedorick <kaylee.fedorick@gmail.com>
2025-10-28 15:09:33 -0600

    Add zen policies

--- modules/home-manager/zen-browser.nix
+++ modules/home-manager/zen-browser.nix
@@ -8,5 +8,27 @@ inputs:
   imports = [ inputs.zen-browser.homeModules.twilight ];
   programs.zen-browser = {
     enable = true;
+    profiles."default" = {
+
+    };
+    policies = {
+      AutofillAddressEnabled = true;
+      AutofillCreditCardEnabled = false;
+      DisableAppUpdate = true;
+      DisableFeedbackCommands = true;
+      DisableFirefoxStudies = true;
+      DisablePocket = true;
+      DisableTelemetry = true;
+      DontCheckDefaultBrowser = true;
+      NoDefaultBookmarks = true;
+      OfferToSaveLogins = false;
+      EnableTrackingProtection = {
+        Value = true;
+        Locked = true;
+        Cryptomining = true;
+        Fingerprinting = true;
+      };
+    };
   };
 }
+
