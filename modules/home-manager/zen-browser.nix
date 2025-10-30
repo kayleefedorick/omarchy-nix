e1bb07ab445828ba6727df7724e0e1a19030aeec
Kaylee Fedorick <kaylee.fedorick@gmail.com>
2025-10-29 18:59:01 -0600

    Add bitwarden to firefox extensions

--- modules/home-manager/zen-browser.nix
+++ modules/home-manager/zen-browser.nix
@@ -20,6 +20,12 @@ inputs:
             installation_mode = "force_installed";
             private_browsing = true;
           };
+          "{446900e4-71c2-419f-a6a7-df9c091e268b}" = {
+            default_area = "menupanel";
+            install_url = "https://addons.mozilla.org/firefox/downloads/latest/bitwarden-password-manager/latest.xpi";
+            installation_mode = "force_installed";
+            private_browsing = false;
+          };
         };
       AutofillAddressEnabled = true;
       AutofillCreditCardEnabled = false;
