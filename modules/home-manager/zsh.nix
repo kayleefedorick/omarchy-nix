f3898e860e3b1fa1255185f7a612e5af6f484a99
Kaylee Fedorick <kaylee.fedorick@gmail.com>
2025-10-29 20:55:47 -0600

    add zsh config

--- modules/home-manager/zsh.nix
+++ modules/home-manager/zsh.nix
@@ -2,7 +2,20 @@
 {
   programs.zsh = {
     enable = true;
+    enableCompletion = true;
     autosuggestion.enable = true;
+    syntaxHighlighting.enable = true;
+
+    shellAliases = {
+      ls = "eza --icons=auto";
+      la = "eza -la --icons=auto";
+      cat = "zsh-bat";
+      update = "cd /etc/nixos && sudo nix flake update omarchy-nix && sudo nixos-rebuild switch";
+    };
+
+    setOptions = [
+      "HIST_IGNORE_ALL_DUPS"
+    ];
     zplug = {
       enable = true;
       plugins = [
