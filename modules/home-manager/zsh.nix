37759b5bd6a628787b339de84577b4a2c95fd540
Kaylee Fedorick <kaylee.fedorick@gmail.com>
2025-10-29 21:14:45 -0600

    modify zsh config

--- modules/home-manager/zsh.nix
+++ modules/home-manager/zsh.nix
@@ -9,25 +9,18 @@
     shellAliases = {
       ls = "eza --icons=auto";
       la = "eza -la --icons=auto";
-      cat = "zsh-bat";
       update = "cd /etc/nixos && sudo nix flake update omarchy-nix && sudo nixos-rebuild switch";
     };
 
     setOptions = [
       "HIST_IGNORE_ALL_DUPS"
     ];
-    zplug = {
-      enable = true;
-      plugins = [
-        {
-          name = "plugins/git";
-          tags = [ "from:oh-my-zsh" ];
-        }
-        {
-          name = "fdellwing/zsh-bat";
-          tags = [ "as:command" ];
-        }
-      ];
+    oh-my-zsh = {
+        enable = true;
+        theme = "robbyrussell";
+        plugins = [
+	        "git"
+        ];
     };
   };
 }
